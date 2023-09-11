23578681a0c4c765813f69ad559b9469afdb9673
ZafarSadik <zafarhdsadik@gmail.com>
2023-09-11 21:12:48 +0530

    First commit

--- tests/unit/test_aws_cdk_tour_planner_stack.py
+++ tests/unit/test_aws_cdk_tour_planner_stack.py
@@ -1,7 +1,7 @@
 import aws_cdk as core
 import aws_cdk.assertions as assertions
 
-from src.aws_cdk_tour_planner_stack import AwsCdkTourPlannerStack
+from aws_cdk_tour_planner.aws_cdk_tour_planner_stack import AwsCdkTourPlannerStack
 
 # example tests. To run these tests, uncomment this file along with the example
 # resource in aws_cdk_tour_planner/aws_cdk_tour_planner_stack.py
