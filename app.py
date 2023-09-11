061dbd9146a70f5996bbfbeca2a81837a6b3b13e
ZafarSadik <zafarhdsadik@gmail.com>
2023-09-11 21:36:09 +0530

    Pipeline Setup

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
-import os
-
 import aws_cdk as cdk
 
-from aws_cdk_tour_planner.aws_cdk_tour_planner_stack import AwsCdkTourPlannerStack
+from src.aws_cdk_tour_planner_stack import AwsCdkTourPlannerStack
 
 
 app = cdk.App()
